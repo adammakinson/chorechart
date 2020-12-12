e90cf81f5fefae2afd66ac57a3184db86d49c700
Adam Makinson <adam.levi.makinson@gmail.com>
2020-12-12 17:24:25 -0500

    Add chores route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChoresApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->apiResource('/chores', ChoresApiController::class);
+// Route::apiResource('/chores', ChoresApiController::class);
