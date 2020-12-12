e30af6704396901122cd25c755a1b5d2996ce58b
Adam Makinson <adam.levi.makinson@gmail.com>
2020-12-12 12:26:28 -0500

    Add AuthController with login function

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * login
+     */
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'username' => 'required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['username', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status_code' => 500,
+                    'message' => 'Unauthorized'
+                ]);
+            }
+
+            $user = User::where('username', $request->username)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Error logging in');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status_code' => 200,
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer'
+            ]);
+
+        } catch (Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error logging in',
+                'error' => $error
+            ]);
+        }
+    }
+}
