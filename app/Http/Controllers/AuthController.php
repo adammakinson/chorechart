2b45722df688f429666051358e9cbe3c2e607d1e
Adam Makinson <adam.levi.makinson@gmail.com>
2020-12-12 13:57:12 -0500

    Add user registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,4 +51,43 @@ class AuthController extends Controller
             ]);
         }
     }
+
+    public function register(Request $request)
+    {
+        try {
+            
+            // Validate the request
+            $request->validate([
+                'name' => 'string',
+                'username' => 'required',
+                'email' => 'email',
+                'password' => 'required',
+                'confirm_password' => 'required'
+            ]);
+
+            $password = $request->password;
+            $passwordConfirmation = $request->confirm_password;            
+            if ($password !== $passwordConfirmation) {
+                throw new \Exception('Your password and password confirmation need to match!');
+            }
+
+            // Create a new instance of the User model passing in the values
+            $user = new User();
+
+            $user->name = $request->name;
+            $user->username = $request->username;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+
+            // Persist the user model to the database
+            $user->save();
+
+        } catch(Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error registering',
+                'error' => $error
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\AuthController;
 |
 */
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
