cadea1dfd9553c27ba59b7a5bf4a38465edf8953
Adam Makinson <adam.levi.makinson@gmail.com>
2020-12-25 07:45:37 -0500

    Make seeder for UserChores

---
+++ database/seeders/UserChoreSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserChoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        /**
+         * This is a pretty awful way to do it.
+         * I should be able to fetch the user and the role
+         * and seed the role_user table with that.
+         * 
+         * This should "work" for this prototype stage though.
+         */
+        DB::table('user_chores')->insert([
+            [
+            'chore_id' => '1',
+            'user_id' => '1',
+            'created_at' => now(),
+            'updated_at' => now()
+            ],
+            [
+            'chore_id' => '2',
+            'user_id' => '2',
+            'created_at' => now(),
+            'updated_at' => now()
+            ]
+        ]);
+    }
+}
