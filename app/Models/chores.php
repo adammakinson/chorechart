2e9e570112329553d1155f4dfce0bd2ace27a3d2
Adam Makinson <adam.levi.makinson@gmail.com>
2021-01-27 22:23:12 -0500

    Return pivot table data from the chores model

--- app/Models/chores.php
+++ app/Models/chores.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Gate;
 
 class chores extends Model
 {
@@ -22,6 +23,14 @@ class chores extends Model
     ];
 
     public function user() {
-        return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id');
+        if (Gate::allows('manage-chorechart')) {
+            return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id')->withPivot('inspection_ready', 'inspected_on', 'inspection_passed');
+        } else {
+            return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id')->withPivot('inspection_ready');
+        }
+    }
+
+    public function assigner() {
+        return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'assigner_id');
     }
 }
