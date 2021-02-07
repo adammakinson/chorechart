faaf60bded70acf3b123f1e730e4983a240b4089
Adam Makinson <adam.levi.makinson@gmail.com>
2021-02-07 18:29:15 -0500

    make transaction seeder

---
+++ database/seeders/transactionSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class transactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('transactions')->insert([
+            [
+            'user_id' => '2',
+            'event' => 'completed a chore',
+            'user_points' => '10',
+            'occurred_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now()
+            ]
+        ]);
+    }
+}
