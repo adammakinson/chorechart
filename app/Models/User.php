edaba825e1c066864d54a212c002d40376790bfa
Adam Makinson <adam.levi.makinson@gmail.com>
2021-08-27 21:38:53 -0400

    rename roles model to role

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Roles::class, 'role_user', 'user_id', 'role_id');
+        return $this->belongsToMany(Role::class);
     }
 
     public function chores()
