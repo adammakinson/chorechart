4264132fd693d37363e67670c5d77a569377813d
Adam Makinson <adam.levi.makinson@gmail.com>
2021-10-03 15:23:03 -0400

    Add comments to the api routes file

--- routes/api.php
+++ routes/api.php
@@ -21,33 +21,35 @@ use App\Http\Controllers\TransactionsController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::put('/update-credentials/{userid}', [AuthController::class, 'updateUserCredentials']);
+Route::middleware('auth:sanctum')->apiResource('/users', UsersController::class);
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-// Route::middleware('auth:sanctum')->put('/users/{assigneeId}/chores/{choreId}', [UserChoresController::class, 'update']);
-
-Route::middleware('auth:sanctum')->get('/users/{assigneeId}/transactions', [TransactionsController::class, 'getUserTransactionsOrderedByCreationTime']);
-
-Route::middleware('auth:sanctum')->post('/users/{assigneeId}/transactions', [TransactionsController::class, 'store']);
+/**
+ * Resource route for chores that takes care of GET, POST PUT/PATCH, and DELETE
+ * db table - chores
+ */
+Route::middleware('auth:sanctum')->apiResource('/chores', ChoresApiController::class);
 
 /**
  * Assigns a chore to a user
+ * db table - user_chores
  */
 Route::middleware('auth:sanctum')->post('/users/{assineeId}/chores/{choreId}', [UserChoresController::class, 'store']);
 
+/**
+ * Updates a chore assigned to a user
+ * db table - user_chores
+ */
 Route::middleware('auth:sanctum')->put('/users/{assigneeId}/chores/{choreId}', [UserChoresController::class, 'update']);
 
-Route::middleware('auth:sanctum')->apiResource('/users', UsersController::class);
+/**
+ * Gets user transactions ordered by creation time
+ * db table - transactions
+ */
+Route::middleware('auth:sanctum')->get('/users/{assigneeId}/transactions', [TransactionsController::class, 'getUserTransactionsOrderedByCreationTime']);
 
-Route::middleware('auth:sanctum')->apiResource('/chores', ChoresApiController::class);
-// Route::apiResource('/chores', ChoresApiController::class);
-
-// How does a user submit their chore for "inspection"?
-// route: POST /chores/{id}?
-// since creating a new chore is a POST to /chores to add to the collection,
-
- // What would a UserChoresController look like?
-    // PUT /users/{id}/chores/{id}
-        // This would get the UserChores model associated with user_id and chore_id
-        // and update EITHER "inspection_ready" OR "inspected_on" + "inspection_passed"
+/**
+ * Creates a new transaction. A transaction is created from the most recent one
+ * IF a chore has been awarded points.
+ * db table - transactions
+ */
+Route::middleware('auth:sanctum')->post('/users/{assigneeId}/transactions', [TransactionsController::class, 'store']);
