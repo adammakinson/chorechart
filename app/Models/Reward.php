44a6998a2daa21b25ca63c456fc7361800c70784
Adam Makinson <adam.levi.makinson@gmail.com>
2021-10-06 21:57:30 -0400

    add reward model

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
 
 class Reward extends Model
 {
     use HasFactory;
+
+    // reward images relationship
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
