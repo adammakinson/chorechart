cd92b64820b91937cae85f10647d5610bad8de57
Adam Makinson <adam.levi.makinson@gmail.com>
2021-10-06 21:57:17 -0400

    add images migration and model

---
+++ database/migrations/2021_10_07_011059_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reward_id');
+            $table->string('path');
+            $table->string('filename');
+            $table->string('file_extension');
+            $table->string('alt_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
