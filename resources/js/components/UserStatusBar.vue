2798e6f4c562cf2c1ac6a70d8a38c01b08468d93
Adam Makinson <adam.levi.makinson@gmail.com>
2021-10-12 22:47:34 -0400

    fetch transactions in the user status bar

--- resources/js/components/UserStatusBar.vue
+++ resources/js/components/UserStatusBar.vue
@@ -15,11 +15,30 @@ export default {
 
     mounted() {
         this.authenticatedUser = !!this.$store.getters.getUserAuthToken;
+
+        if (!this.$store.getters.getUserTransactions || this.$store.getters.getUserTransactions.length == 0) {
+            this.fetchUsersTransactions();
+        }
         
         if(this.authenticatedUser) {
             this.usersName = this.$store.getters.getUsersName;
         } else {
             this.logout();
+        }
+    },
+
+    methods: {
+        fetchUsersTransactions() {
+            let user = this.$store.getters.getUser;
+
+            axios.get('/api/users/' + user.id + '/transactions', {
+                headers: {
+                    authorization: this.$store.getters.getUserAuthToken
+                }
+            }).then((response) => {
+
+                this.$store.commit('setUserTransactions', response.data);
+            });
         }
     }
 }
