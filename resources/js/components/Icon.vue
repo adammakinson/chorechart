cb7ff888738117678cfb9b1f87c81c6f8db95b1c
Adam Makinson <adam.levi.makinson@gmail.com>
2021-10-25 23:18:43 -0400

    make an icon component

---
+++ resources/js/components/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+    <span v-on:click="handleIconClick"></span>
+</template>
+    
+<script>
+import eventBus from '../eventBus';
+
+export default {
+    /**
+     * The thought is that the icon "component" will be generic and accept
+     * the class that makes the icon from props as well as the event the
+     * icon should emit when clicked.
+     */
+
+    methods: {
+        handleIconClick(e) {
+            // eventBus.$emit('reward-card-click', this.cardData);
+            e.stopPropagation();
+            eventBus.$emit(this.$attrs.iconevent, this);
+            // console.log(this);
+        }
+    }
+}
+</script>
