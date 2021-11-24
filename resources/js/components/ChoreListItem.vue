639b30af4d8ca0797ee22fbb3c688fa40c07a6d4
Adam Makinson <adam.levi.makinson@gmail.com>
2021-11-23 23:06:31 -0500

    Add initial methods to handle chore items clicks

--- resources/js/components/ChoreListItem.vue
+++ resources/js/components/ChoreListItem.vue
@@ -1,12 +1,12 @@
 <template>
-    <li class="list-group-item">
+    <li class="list-group-item" v-bind:data-itemId="listItem.id" v-on:click="handleChoreClick">
         <div style="display:flex; justify-content: space-between">
             <div>
                 {{listItem.title}}
             </div>
             <div>
-                <span class="fas fa-edit text-info"></span>
-                <span class="fas fa-trash text-danger"></span>
+                <span class="fas fa-edit text-info" v-bind:data-itemId="listItem.id" v-on:click.stop="handleChoreEditClick"></span>
+                <span class="fas fa-trash text-danger" v-bind:data-itemId="listItem.id" v-on:click.stop="handleChoreDeleteClick"></span>
             </div>
         </div>
     </li>
@@ -21,6 +21,23 @@ export default {
     data() {
         return {
 
+        }
+    },
+
+    methods: {
+        handleChoreClick(event) {
+            let choreItem = event.target.closest('.list-group-item');
+
+            choreItem.classList.toggle('active');
+           console.log('clicked a general chore item');
+        },
+
+        handleChoreEditClick() {
+            console.log('clicked a chore edit button');
+        },
+
+        handleChoreDeleteClick() {
+            console.log('clicked a chore delete button');
         }
     }
 }
