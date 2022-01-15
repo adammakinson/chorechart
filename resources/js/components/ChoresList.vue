4b169bb15678ee0b64d7e026a4e038d2f137e79d
Adam Makinson <adam.levi.makinson@gmail.com>
2022-01-15 12:44:24 -0500

    remove functions no longer needed in the ChoresList view

--- resources/js/components/ChoresList.vue
+++ resources/js/components/ChoresList.vue
@@ -2,13 +2,11 @@
     <div>
         <appmenu></appmenu>
         <user-status-bar></user-status-bar>
-        <button v-if="userIsAdmin" class="btn btn-primary" data-toggle="modal" data-target="#createChoreModal" v-on:click="addChore">Add chore</button>
         <hr>
         <datatable :columns="columns" :data="rows">
             <template slot-scope="{row, columns}">
                 <tr v-bind:id="row.id">
                     <td>{{row.chore}}</td>
-                    <td v-if="userIsAdmin">{{row.assignedUsers}}</td>
                     <td>{{row.pointvalue}}</td>
                     <td>
                         <div class="actionsContainer">
@@ -23,73 +21,10 @@
                 </tr>
             </template>
         </datatable>
-        <modal id="createChoreModal" v-if="userIsAdmin">
-            <template v-slot:header>
-                Create a chore
-            </template>
-            <div class="modal-body">
-                <form id="createChoreForm">
-                    <div class="form-group">
-                        <label for="chore">Chore:</label>
-                        <input id="chore" name="chore" class="form-control" type="text" v-bind:value="choreFieldValue">
-                    </div>
-                    <div class="form-group">
-                        <label for="pointvalue">Point value:</label>
-                        <input id="pointvalue" name="pointvalue" class="form-control" type="number" v-bind:value="pointFieldValue">
-                    </div>
-                    <div class="form-group">
-                        <label for="assignedto">Assigned to:</label>
-                        <!-- <input id="assignedto" name="assignedto" class="form-control" type="number" v-bind:value="pointFieldValue"> -->
-                        <select class="form-select form-control" name="assignedto" aria-label="Assigned to user">
-                            <option value="" selected>Unassigned</option>
-                            <option v-for="user in allUsers" :key="user.id" v-bind:value="user.id">{{user.name}}</option>
-                        </select>
-                    </div>
-                </form>
-            </div>
-            <template v-slot:footer>
-                <footer class="modal-footer">
-                    <button type="button" class="btn btn-primary" v-on:click.prevent="createChore">Create chore</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal" v-on:click.prevent="sendEventBusMessage">Close</button>
-                </footer>
-            </template>
-        </modal>
-        <modal id="editChoreModal" v-if="userIsAdmin">
-            <template v-slot:header>
-                Edit chore
-            </template>
-            <div class="modal-body">
-                <form id="editChoreForm">
-                    <div class="form-group">
-                        <label for="editchore">Chore:</label>
-                        <input id="editchore" class="form-control" type="text" name="chore" v-bind:value="choreFieldValue">
-                    </div>
-                    <div class="form-group">
-                        <label for="editpointvalue">Point value:</label>
-                        <input id="editpointvalue" class="form-control" type="number" name="pointvalue" v-bind:value="pointFieldValue">
-                    </div>
-                    <div class="form-group">
-                        <label for="editassignedto">Assigned to:</label>
-                        <!-- <input id="editassignedto" name="assignedto" class="form-control" type="number" v-bind:value="pointFieldValue"> -->
-                        <select id="editassignedto" class="form-select form-control" name="assignedto" aria-label="Assigned to user">
-                            <option value="''" v-bind:selected="assignee == ''">Unassigned</option>
-                            <option v-for="user in allUsers" :key="user.id" v-bind:value="user.id" v-bind:selected="assignee == user.id">{{user.name}}</option>
-                        </select>
-                    </div>
-                </form>
-            </div>
-            <template v-slot:footer>
-                <footer class="modal-footer">
-                    <button type="button" class="btn btn-primary" v-on:click.prevent="updateChore">Update chore</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal" v-on:click.prevent="sendEventBusMessage">Close</button>
-                </footer>
-            </template>
-        </modal>
     </div>
 </template>
 
 <script>
-import Modal from "./Modal";
 import Appmenu from './AppMenu.vue';
 import UserStatusBar from './UserStatusBar.vue';
 import eventBus from '../eventBus';
@@ -120,7 +55,6 @@ export default {
     },
 
     components: {
-        Modal,
         UserStatusBar,
         Appmenu
     },
@@ -128,16 +62,9 @@ export default {
     mounted() {
 
         if (this.$store.getters.getUserAuthToken) {
-
-            this.userIsAdmin = this.$store.getters.userIsAdmin;
-            
             this.fetchChoresCollection();
 
             this.fetchUsersTransactions();
-
-            if(this.userIsAdmin) {
-                this.getAllUsers();
-            }
         } else {
 
             this.$router.push('login');
@@ -190,27 +117,6 @@ export default {
             data.forEach((row) => {
 
                 row.submittable = false;
-                row.editable = true;
-                row.deletable = true;
-
-                if (row.user && row.user.length) {
-                    row.user = row.user[0];
-                    row.pivot = row.user.pivot;
-                }
-                
-                if (this.userIsAdmin) {
-                    
-                    if(row.assigner && row.assigner.length) {
-                        row.assigner = row.assigner[0];
-                    }
-
-                    // Hrm, I think the thought here was if a chore was assigned to multiple users. I'm not sure I want 
-                    // to go that route, but rather perhaps have chore instances? would need to stay an array for reduce 
-                    // to work
-                    // row.assignedUsers = row.user.reduce((total, user) => {
-                    //     return user.name + ', ';
-                    // }, '');
-                    row.assignedUsers = row.user.name;
 
                 if(!this.choreIsFinished(row)) {
                     if (this.choreIsSelfAssigned(row) || this.choreIsReadyForInspection(row)){
@@ -218,14 +124,7 @@ export default {
                     }
                 }
 
-                    if(!row.user || Array.isArray(row.user)){
-                        row.user = {};
-                    }
 
-                    if(!row.user.id && row.pivot){
-                        row.user.id = row.pivot.user_id;
-                    }
-                }
             });
 
             return data;
@@ -248,68 +147,21 @@ export default {
             this.columns = [
                 {label: 'chore', field: 'chore'}
             ];
-
-            if(this.userIsAdmin) {
-                this.columns.push({label: 'assigned', field: 'assignedUsers'});
-            }
-            
             this.columns.push({label: 'points', field: 'pointvalue'});
 
             this.rows = data;
         },
 
-        getAllUsers() {
-            axios.get('/api/users', {
-                headers: {
-                    authorization: this.$store.getters.getUserAuthToken
-                }
-            }).then(
-                (response) => {
-                    this.allUsers = response.data;
-                }
-            );
-        },
-
-        addChore() {
-            let modalwindow = document.getElementById("createChoreModal");
-
-            this.choreFieldValue = '';
-            this.pointFieldValue = '';
-            this.assignee = '';
-            this.activeElementId = '';
-
-            modalwindow.style.display = 'block';
-        },
 
         findChoreById(allChores, choreId) {
             return allChores.find(chore => chore.id == choreId);
         },
 
-        editChore(el) {
-            let modalwindow = document.getElementById("editChoreModal");
-            let choreId = el.target.dataset.choreid;
-            let allChores = this.chores;
-            let choreBeingEdited = this.findChoreById(allChores, choreId);
-
-            this.choreFieldValue = choreBeingEdited.chore;
-            this.pointFieldValue = choreBeingEdited.pointvalue;
-
-            if(choreBeingEdited.user && choreBeingEdited.user.length > 0) {
-                this.assignee = choreBeingEdited.user[0].id;
-            } else {
-                this.assignee = '';
-            }
-
-            this.activeElementId = choreId;
-
-            modalwindow.style.display = 'block';
-        },
 
         handleCheckClick(el) {
             let choreId = el.target.dataset.choreid;
             let allChores = this.chores;
             let choreBeingEdited = this.findChoreById(allChores, choreId);
-            let user = this.$store.getters.getUser;
             let choreData = {
                 id: choreBeingEdited.id,
                 userId: choreBeingEdited.user_id,
@@ -363,90 +215,6 @@ export default {
             this.$store.commit('setUserTransactions', transactions);
         },
 
-        deleteChore(el) {
-            let itemId = el.target.dataset.choreid;
-
-            axios.delete('/api/chores/' + itemId, {
-                headers: {
-                    authorization: this.$store.getters.getUserAuthToken
-                }
-            }).then((response) => {
-                this.fetchChoresCollection();
-            });
-        },
-
-        createChore(){
-            let formEls = document.querySelectorAll('#createChoreModal .form-control');
-            let choreData = {};
-
-            formEls.forEach((el) => {
-                choreData[el.name] = el.value;
-            });
-
-            // Create the chore
-            axios({
-                method: 'post',
-                url: '/api/chores',
-                data: choreData,
-                headers: {
-                    authorization: this.$store.getters.getUserAuthToken
-                }
-            }).then((response) => {
-
-                let chore = response.data;
-
-                // If a user has been specified in the form,
-                // assign the chore to the user.
-                if (choreData['assignedto'] !== '') {
-                    axios({
-                        method: 'post',
-                        url: '/api/users/' + choreData['assignedto'] + '/chores/' + chore.id,
-                        data: choreData,
-                        headers: {
-                            authorization: this.$store.getters.getUserAuthToken
-                        }
-                    });
-                }
-
-            }).then(() => {
-                this.fetchChoresCollection();
-                
-                // Close the modal
-                eventBus.$emit('close-modal');
-            });
-        },
-        
-        updateChore() {
-            let formEls = document.querySelectorAll('#editChoreModal .form-control');
-            let choreData = {};
-
-            formEls.forEach((formInput) => {
-                choreData[formInput.name] = formInput.value;
-            });
-
-            axios({
-                method: 'put',
-                url: '/api/chores/' + this.activeElementId,
-                data: choreData,
-                headers: {
-                    authorization: this.$store.getters.getUserAuthToken
-                }
-            }).then((response) => {
-
-                this.chores = this.processFetchedChores(response.data);
-
-                this.rows = this.chores;
-                
-                this.activeElementId = '';
-
-                // Close the modal
-                eventBus.$emit('close-modal');
-            });
-        },
-
-        sendEventBusMessage() {
-            eventBus.$emit('close-modal');
-        }
     }
 };
 </script>
