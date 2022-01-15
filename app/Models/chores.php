2f87b6d5bd6b934ad740977f422990b8374d2303
Adam Makinson <adam.levi.makinson@gmail.com>
2022-01-15 13:09:40 -0500

    clean up no longer needed model relationships

--- app/Models/chores.php
+++ app/Models/chores.php
@@ -25,21 +25,4 @@ class chores extends Model
         'pointvalue'
     ];
 
-    public function user() {
-        if (Gate::allows('manage-chorechart')) {
-            return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id')->withPivot('inspection_ready', 'inspected_on', 'pending', 'inspection_passed', 'points_awarded');
-        } else {
-            return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id')->withPivot('inspection_ready', 'pending', 'inspection_passed', 'points_awarded');
-        }
-        
-        // if (Gate::allows('manage-chorechart')) {
-        //     return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id')->using(UserChores::class);
-        // } else {
-        //     return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'user_id')->using(UserChores::class);
-        // }
-    }
-
-    public function assigner() {
-        return $this->belongsToMany(User::class, 'user_chores', 'chore_id', 'assigner_id');
-    }
 }
