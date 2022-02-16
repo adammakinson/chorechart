7f5a862f3d5b9457054478ce5c090090860347f5
Adam Makinson <adam.levi.makinson@gmail.com>
2022-02-15 21:10:34 -0500

    make the card component more generic so that it can support multiple use cases

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,36 +1,15 @@
 <template>
-    <div v-on:click="handleCardClick" class="card p-1 border rounded bg-light justify-content-between" v-bind:data-itemId="cardData.id" v-bind:data-eventToFire="cardData.eventToFire">
-        <img v-for="image in cardData.images" :key="image.id" :src="image.path+image.filename" :alt="cardData.imgalt">
-        <h4 class="text-dark">{{cardData.title}}</h4>
-        <h5><b>cost: </b>{{cardData.cost}}</h5>
-        <div v-if="cardData.actionIcons" class="flex-column m-3">
-            <icon v-for="actionicon in cardData.actionIcons" :key="actionicon.event" 
-                v-bind:class="actionicon.class" 
-                v-bind:iconevent="actionicon.event" 
-                v-bind:data-itemId="cardData.id">
-            </icon>
-        </div>
+    <div v-on="cardData['eventsObject']" class="card p-1 border rounded bg-light" v-bind:data-itemId="cardData.id" v-bind:data-eventToFire="cardData.eventToFire">
+        <slot></slot>
     </div>
 </template>
 
 <script>
-import eventBus from '../eventBus';
-import icon from '../components/Icon';
-export default {
 
-    components: {
-        icon
-    },
+export default {
 
     props: [
         'cardData'
-    ],
-
-    methods: {
-        handleCardClick(e) {
-            let eventToFire = e.target.closest('.card').getAttribute('data-eventToFire');
-            eventBus.$emit(eventToFire, this.cardData);
-        }
-    }
+    ]
 }
 </script>
