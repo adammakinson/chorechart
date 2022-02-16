3dd6d80fac18f21df290a92f0f1dce4cd3fff36c
Adam Makinson <adam.levi.makinson@gmail.com>
2022-02-15 20:56:55 -0500

    remove choreListItem and create generic listItem instead

---
+++ resources/js/components/ListItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <li :id="'list-item_'+listItem.id" class="list-group-item" v-bind:data-itemId="listItem.id" v-on:click="handleChoreClick" v-on:dragstart="dragStart" draggable="true" style="display:flex; justify-content: space-between">
+        <slot></slot>
+        <div>
+            <slot name="actions"></slot>
+        </div>
+    </li>
+</template>
+
+<script>
+import eventBus from "../eventBus.js";
+
+export default {
+    props: [
+        'listItem'
+    ],
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+        handleChoreClick(event) {
+            let choreItem = event.target.closest('.list-group-item');
+
+            choreItem.classList.toggle('active');
+        },
+
+        dragStart(e) {
+            e.dataTransfer.setData('text/html', e.target.innerText);
+            e.dataTransfer.setData('text/choreId', e.target.dataset.itemid);
+        },
+    }
+}
+</script>
