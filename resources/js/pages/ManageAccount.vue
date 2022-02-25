df19936cf3693dfa7b078602aea1a66f94659506
Adam Makinson <adam.levi.makinson@gmail.com>
2022-02-24 23:32:24 -0500

    make UI adjustments

--- resources/js/pages/ManageAccount.vue
+++ resources/js/pages/ManageAccount.vue
@@ -1,34 +1,33 @@
 <template>
     <div>
         <appmenu></appmenu>
-        <user-status-bar></user-status-bar>
         <h1>Manage users</h1>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row justify-content-center">
                 <div id="errorMessage">
                     <p v-for="errorMessage in errorMessages" :key="errorMessage.message">{{errorMessage.message}}</p>
                 </div>
             </div>
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mb-4">
                 <div class="card" style="width:40em;">
                     <div class="card-header">
                         <h1>User information</h1>
                     </div>
                     <div class="card-body">
                         <form id="editUserForm">
-                            <div class="form-group">
+                            <div class="form-group mb-2">
                                 <label for="name">Name:</label>
                                 <input id="name" name="name" class="form-control" type="text" v-model="this.userData.name">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mb-2">
                                 <label for="username">Username:</label>
                                 <input id="username" name="username" class="form-control" type="text" v-model="this.userData.username">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mb-2">
                                 <label for="email">Email:</label>
                                 <input id="email" name="email" class="form-control" type="text" v-model="this.userData.email">
                             </div>
-                            <div>
+                            <div class="form-group">
                                 <button type="button" class="btn btn-primary" v-on:click.prevent="updateUser">Update user</button>
                             </div>
                         </form>
@@ -46,15 +45,15 @@
                                 <label for="uname">Username:</label>
                                 <input id="uname" name="username" class="form-control" type="text" v-bind:value="editingUsernameValue">
                             </div> -->
-                            <div class="form-group">
+                            <div class="form-group mb-2">
                                 <label for="password">Password:</label>
                                 <input id="password" name="password" class="form-control" type="password" value="">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mb-2">
                                 <label for="confirm_password">Confirm password:</label>
                                 <input id="confirm_password" name="confirm_password" class="form-control" type="password" value="">
                             </div>
-                            <div>
+                            <div class="form-group">
                                 <button type="button" class="btn btn-primary" v-on:click.prevent="changeUserCredentials">Update credentials</button>
                             </div>
                         </form>
@@ -68,15 +67,14 @@
 <script>
 import Card from '../components/Card.vue';
 import Appmenu from '../components/AppMenu.vue';
-import UserStatusBar from '../components/UserStatusBar.vue';
 
 export default {
-  components: { Card, Appmenu, UserStatusBar },
+  components: { Card, Appmenu },
   
   data() {
       return {
           userData: {},
-          errorMessages = []
+          errorMessages: []
       }
   },
 
