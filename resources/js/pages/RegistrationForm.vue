ab9395a317f3395362cbd53cde8485ceeb7f538a
Adam Makinson <adam.levi.makinson@gmail.com>
2022-02-26 14:48:13 -0500

    Adjustments to the registration form layout

--- resources/js/pages/RegistrationForm.vue
+++ resources/js/pages/RegistrationForm.vue
@@ -1,36 +1,38 @@
 <template>
     <div class="container d-flex align-items-center justify-content-center" style="height: 100vh">
-        <div class="card" style="width: 40em;">
-            <div class="card-header">
-                <h1>Register</h1>
-            </div>
-            <div class="card-body">
-                <form id="loginForm">
-                    <div class="form-group mb-3">
-                        <label for="name">Name:</label>
-                        <input type="text" name="name" v-model="name" id="name" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="username">Username:</label>
-                        <input type="text" name="username" v-model="username" id="username" class="form-control" required>
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="email">Email:</label>
-                        <input type="text" name="email" v-model="email" id="email" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" v-model="password" id="password" class="form-control" required>
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="passwordconfirm">Confirm password:</label>
-                        <input type="password" name="passwordconfirm" v-model="passwordconfirm" id="passwordconfirm" class="form-control" required>
-                    </div>
-                    <div class="form-group mb-3">
-                        <button class="btn btn-primary" v-on:click.prevent="handleRegistration" type="submit">Register</button>
-                        <p v-if="error">{{error}}</p>
-                    </div>
-                </form>
+        <div class="row">
+            <h1>Register</h1>
+            <div class="card">
+                <div class="card-header">
+                </div>
+                <div class="card-body">
+                    <form id="loginForm">
+                        <div class="form-group mb-3">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" v-model="name" id="name" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="username">Username:</label>
+                            <input type="text" name="username" v-model="username" id="username" class="form-control" required>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="email">Email:</label>
+                            <input type="text" name="email" v-model="email" id="email" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password">Password:</label>
+                            <input type="password" name="password" v-model="password" id="password" class="form-control" required>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="passwordconfirm">Confirm password:</label>
+                            <input type="password" name="passwordconfirm" v-model="passwordconfirm" id="passwordconfirm" class="form-control" required>
+                        </div>
+                        <div class="form-group mb-3">
+                            <button class="btn btn-primary" v-on:click.prevent="handleRegistration" type="submit">Register</button>
+                            <p v-if="error">{{error}}</p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
