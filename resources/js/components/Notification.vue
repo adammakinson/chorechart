14427e70a99c2e2688f430cf781e88885a156f80
Adam Makinson <adam.levi.makinson@gmail.com>
2022-03-05 07:23:36 -0500

    refactor the notification component a bit

--- resources/js/components/Notification.vue
+++ resources/js/components/Notification.vue
@@ -21,18 +21,43 @@ export default {
             
             // I want to use success, danger, and info primarily
             // If the type is specified, use that.
-            if (this.notice.type == 'error') {
-                notificationColorClass = 'alert-danger';
+            if (this.notice.type) {
+                notificationColorClass = this.getNotificationColorClassFromType(this.notice.type);
             }
 
-            // If a type is not specified, and the notification has a status code,
-            // use a status code mapping to determine what should be returned.
-            if (this.notice.status == 403) {
+            if (this.notice.status) {
+                notificationColorClass = this.getNotificationColorClassFromStatus(this.notice.status);
+            }
+
+            return notificationColorClass;
+        },
+
+        getNotificationColorClassFromType(noticetype) {
+            let notificationColorClass = 'alert-info';
+            
+            if(noticetype == 'success'){
+                notificationColorClass = 'alert-success';
+            } else if (noticetype == 'error') {
                 notificationColorClass = 'alert-danger';
             }
 
             return notificationColorClass;
-        }
+        },
+
+        getNotificationColorClassFromStatus(status) {
+            let statusNumber = parseInt(status);
+            let notificationColorClass = 'alert-info';
+
+            if(statusNumber > 199 && statusNumber < 300) {
+                notificationColorClass = 'alert-success';
+            }
+
+            if(statusNumber >= 400) {
+                notificationColorClass = 'alert-danger';
+            }
+
+            return notificationColorClass;
+        },
     }
 }
 </script>
