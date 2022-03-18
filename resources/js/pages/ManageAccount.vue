08ee35dc62ccdf8ff5eb3f9881bd25a05a5a3b62
Adam Makinson <adam.levi.makinson@gmail.com>
2022-03-18 19:21:25 -0400

    add notifications and errors to the account management page

--- resources/js/pages/ManageAccount.vue
+++ resources/js/pages/ManageAccount.vue
@@ -3,28 +3,28 @@
         <appmenu></appmenu>
         <h1>Manage users</h1>
         <div class="container-fluid">
-            <div class="row justify-content-center">
-                <div id="errorMessage">
-                    <p v-for="errorMessage in errorMessages" :key="errorMessage.message">{{errorMessage.message}}</p>
+            <div class="row justify-content-center mb-4">
+                <div style="width:40em;">
+                    <h1>User information</h1>
                 </div>
             </div>
             <div class="row justify-content-center mb-4">
                 <div class="card" style="width:40em;">
                     <div class="card-header">
-                        <h1>User information</h1>
+                        <notification v-if="typeof userInfoErrorsNotice === 'object'" v-bind:notice="userInfoErrorsNotice"></notification>
                     </div>
                     <div class="card-body">
                         <form id="editUserForm">
                             <div class="form-group mb-2">
-                                <label for="name">Name:</label>
+                                <label for="name">Name: <span class="text-danger text-opacity-50" v-if="errors.name">{{errors.name[0]}}</span></label>
                                 <input id="name" name="name" class="form-control" type="text" v-model="userData.name">
                             </div>
                             <div class="form-group mb-2">
-                                <label for="username">Username:</label>
+                                <label for="username">Username: <span class="text-danger text-opacity-50" v-if="errors.username">{{errors.username[0]}}</span></label>
                                 <input id="username" name="username" class="form-control" type="text" v-model="userData.username">
                             </div>
                             <div class="form-group mb-2">
-                                <label for="email">Email:</label>
+                                <label for="email">Email: <span class="text-danger text-opacity-50" v-if="errors.email">{{errors.email[0]}}</span></label>
                                 <input id="email" name="email" class="form-control" type="text" v-model="userData.email">
                             </div>
                             <div class="form-group">
@@ -34,23 +34,24 @@
                     </div>
                 </div>
             </div>
+            <div class="row justify-content-center mb-4">
+                <div style="width:40em;">
+                    <h1>Credentials</h1>
+                </div>
+            </div>
             <div class="row justify-content-center">
                 <div class="card" style="width:40em;">
                     <div class="card-header">
-                        <h1>Credentials</h1>
+                        <notification v-if="typeof userCredsErrorsNotice === 'object'" v-bind:notice="userCredsErrorsNotice"></notification>
                     </div>
                     <div class="card-body">
                         <form id="changeUserCredentialsForm">
-                            <!-- <div class="form-group">
-                                <label for="uname">Username:</label>
-                                <input id="uname" name="username" class="form-control" type="text" v-bind:value="editingUsernameValue">
-                            </div> -->
                             <div class="form-group mb-2">
-                                <label for="password">Password:</label>
+                                <label for="password">Password: <span class="text-danger text-opacity-50" v-if="errors.password">{{errors.password[0]}}</span></label>
                                 <input id="password" name="password" class="form-control" type="password" value="">
                             </div>
                             <div class="form-group mb-2">
-                                <label for="confirm_password">Confirm password:</label>
+                                <label for="confirm_password">Confirm password: <span class="text-danger text-opacity-50" v-if="errors.confirm_password">{{errors.confirm_password[0]}}</span></label>
                                 <input id="confirm_password" name="confirm_password" class="form-control" type="password" value="">
                             </div>
                             <div class="form-group">
@@ -67,17 +68,21 @@
 <script>
 import Card from '../components/Card.vue';
 import Appmenu from '../components/AppMenu.vue';
+import Notification from '../components/Notification.vue';
 
 export default {
     components: { 
         Card,
-        Appmenu
+        Appmenu,
+        Notification
     },
   
     data() {
         return {
             userData: {},
-            errorMessages: []
+            userInfoErrorsNotice: '',
+            userCredsErrorsNotice: '',
+            errors: {}
         }
     },
 
@@ -87,7 +92,6 @@ export default {
 
     methods: {
         updateUserInfo() {
-            console.log("updating user information");
 
             axios({
                 method: 'put',
@@ -97,9 +101,22 @@ export default {
                     authorization: this.$store.getters.getUserAuthToken
                 }
             }).then((response) => {
-                console.log(response.data.message);
+                // hrm... it really isn't an errors notification, really
+                // its a general one. need to fix that naming convention...
+
+                this.userInfoErrorsNotice = {
+                    message: 'User information successfully updated!',
+                    type: 'success'
+                };
             }).catch((error) => {
-                console.log(error);
+                if (error.response) {
+                    this.userInfoErrorsNotice = {
+                        message: error.response.data.message,
+                        status: error.response.status
+                    };
+
+                    this.errors = error.response.data.errors;
+                }
             });
         },
 
@@ -113,24 +130,43 @@ export default {
             userData.confirm_password = inputPasswordConfirm;
             userData.username = this.userData.username;
 
-            // reset errorMessages to an empty array
-            this.errorMessages = [];
-
             // TODO validate passwords on the fly with an onchange event.
             if (userData['password'].length < 8) {
-                this.errorMessages.push({ message: 'Your password needs to be at least eight characters'});
+                if(!Array.isArray(this.errors.password)) {
+                    this.errors.password = [];
+                }
+
+                this.errors.password.push('You must have at least eight characters');
             }
             
             if (userData['confirm_password'].length < 8) {
-                this.errorMessages.push({ message: 'Your password confirmation needs to be at least eight characters'});
+                if(!Array.isArray(this.errors.confirm_password)) {
+                    this.errors.confirm_password = [];
+                }
+                
+                this.errors.confirm_password.push('You must have at least eight characters');
             }
             
             if (userData['password'] != userData['confirm_password']) {
-                this.errorMessages.push({ message: 'Your passwords do not match'});
+                if(!Array.isArray(this.errors.password)) {
+                    this.errors.password = [];
+                }
+
+                if(!Array.isArray(this.errors.confirm_password)) {
+                    this.errors.confirm_password = [];
+                }
+                
+                this.errors.password.push('Your passwords do not match');
+                this.errors.confirm_password.push('Your passwords do not match');
             }
 
-            if(this.errorMessages.length == 0) {
+            if(this.errors.length == 0) {
                 validPasswords = true;
+            } else {
+                this.userCredsErrorsNotice = {
+                    message: 'The given data was invalid',
+                    type: 'error'
+                };
             }
 
             if(validPasswords) {
@@ -143,16 +179,24 @@ export default {
                         authorization: this.$store.getters.getUserAuthToken
                     }
                 }).then((response) => {
-                    console.log(response.data);
+                    // rename
+                    this.userCredsErrorsNotice = {
+                        message: 'Password updates successfully!',
+                        type: 'success'
+                    };
 
                     document.querySelector('#password').value = '';
                     document.querySelector('#confirm_password').value = '';
                 }).catch((error) => {
-                    // TODO - log a nice message to the user
-                    console.log(error);
+                    if (error.response) {
+                        this.userCredsErrorsNotice = {
+                            message: error.response.data.message,
+                            status: error.response.status
+                        };
+
+                        this.errors = error.response.data.errors;
+                    }
                 });
-            } else {
-                console.log('invalid passwords');
             }
         }
     }
