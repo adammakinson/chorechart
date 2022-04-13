48003cf3e1a93e85dcb2bdab325925738f491570
Adam Makinson <adam.levi.makinson@gmail.com>
2022-04-12 22:40:58 -0400

    create start of link component

---
+++ resources/js/components/Link.vue
@@ -0,0 +1,10 @@
+<template>
+    <a class="text-blue-600 hover:text-blue-400 active:text-purple-400 visited:text-purple-400" href="href"><slot></slot></a>
+</template>
+
+<script>
+export default {
+    props: [
+        'href'
+    ]
+}
--- resources/js/pages/WelcomeScreen.vue
+++ resources/js/pages/WelcomeScreen.vue
@@ -4,16 +4,22 @@
             <h1 class="mx-auto mb-4 text-3xl font-bold">Welcome to the chore chart!</h1>
             <p class="mx-auto">A great way to keep things going around the house!</p>
             <div class="mx-auto mt-4">
-                <a href="/login" class="inline-block border px-4 py-2 shadow-md bg-blue-600 hover:bg-blue-400 text-white rounded-lg">Login</a> 
-                <b>or</b> 
-                <a href="/register" class="inline-block border px-4 py-2 shadow-md bg-blue-600 hover:bg-blue-400 text-white rounded-lg">register</a>
+                <Link href="/login">Login</Link>
+                <b>or</b>
+                <Link href="/register">Register</Link>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Link from '../components/Link.vue';
+
 export default {
+    components: {
+        Link
+    },
+
     mounted() {
         if(this.$store.getters.getUserAuthToken) {
             this.$router.push('chores-list');
