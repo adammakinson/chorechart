2f02714a04ba568cf52a658775aae2bf03f6b31f
Adam Makinson <adam.levi.makinson@gmail.com>
2022-05-07 23:12:13 -0400

    Use the new FormInput element on the registration page

--- resources/js/pages/RegistrationForm.vue
+++ resources/js/pages/RegistrationForm.vue
@@ -2,23 +2,49 @@
     <div class="flex flex-col justify-center h-screen">
         <div class="w-112 self-center">
             <h1 class="ml-4">Register</h1>
-            <div class="mx-4 mt-4 px-4 pb-4 border rounded-sm">
+            <div class="m-4 p-4 border rounded-sm">
                 <notification v-if="typeof registerFormNotification === 'object'" v-bind:notice="registerFormNotification"></notification>
-                <form id="loginForm">
-                    <label for="name" class="block mt-4">Name: <span class="text-red-600" v-if="errors.name">{{errors.name[0]}}</span></label>
-                    <input type="text" name="name" v-model="name" id="name" class="block border h-8 w-full">
-                    
-                    <label for="username" class="block mt-4">Username: <span class="text-red-600" v-if="errors.username">{{errors.username[0]}}</span></label>
-                    <input type="text" name="username" v-model="username" id="username" class="block border h-8 w-full" required>
-
-                    <label for="email" class="block mt-4">Email: <span class="text-red-600" v-if="errors.email">{{errors.email[0]}}</span></label>
-                    <input type="text" name="email" v-model="email" id="email" class="block border h-8 w-full">
-                    
-                    <label for="password" class="block mt-4">Password: <span class="text-red-600" v-if="errors.password">{{errors.password[0]}}</span></label>
-                    <input type="password" name="password" v-model="password" id="password" class="block border h-8 w-full" required>
-
-                    <label for="passwordconfirm" class="block mt-4">Confirm password: <span class="text-red-600" v-if="errors.confirm_password">{{errors.confirm_password[0]}}</span></label>
-                    <input type="password" name="passwordconfirm" v-model="passwordconfirm" id="passwordconfirm" class="block border h-8 w-full" required>
+                <form id="loginForm" class="flex flex-col flex-rows-6 gap-y-4">
+                    <FormInput 
+                        :identifier="nameField.identifier" 
+                        :type="nameField.type" 
+                        :elementLabel="nameField.label" 
+                        :errors="errors.name" 
+                        :value="nameField.value" 
+                        :callback="nameField.callback"
+                    ></FormInput>
+                    <FormInput 
+                        :identifier="userNameField.identifier" 
+                        :type="userNameField.type" 
+                        :elementLabel="userNameField.label" 
+                        :errors="errors.username" 
+                        :value="userNameField.value" 
+                        :callback="userNameField.callback"
+                    ></FormInput>
+                    <FormInput 
+                        :identifier="emailField.identifier" 
+                        :type="emailField.type" 
+                        :elementLabel="emailField.label" 
+                        :errors="errors.email" 
+                        :value="emailField.value" 
+                        :callback="emailField.callback"
+                    ></FormInput>
+                    <FormInput 
+                        :identifier="passwordField.identifier" 
+                        :type="passwordField.type" 
+                        :elementLabel="passwordField.label" 
+                        :errors="errors.password" 
+                        :value="passwordField.value" 
+                        :callback="passwordField.callback"
+                    ></FormInput>
+                    <FormInput 
+                        :identifier="confirmPasswordField.identifier" 
+                        :type="confirmPasswordField.type" 
+                        :elementLabel="confirmPasswordField.label" 
+                        :errors="errors.confirm_password"
+                        :value="confirmPasswordField.value" 
+                        :callback="confirmPasswordField.callback"
+                    ></FormInput>
 
                     <div class="flex justify-between">
                         <Button colorClass="text-white" bgColorClass="bg-blue-600" callback="handleRegistration">Register</Button>
@@ -35,13 +61,15 @@ import Notification from '../components/Notification.vue';
 import Link from '../components/Link.vue';
 import Button from '../components/Button.vue';
 import eventBus from '../eventBus.js';
+import FormInput from '../components/FormInput.vue';
 
 export default {
     components: {
         Notification,
         Link,
         Button,
-        eventBus
+        eventBus,
+        FormInput
     },
 
     created() {
@@ -59,24 +87,55 @@ export default {
 
     data() {
         return {
-            name: '',
-            username: '',
-            email: '',
-            password: '',
-            passwordconfirm: '',
             registerFormNotification: '',
-            errors: ''
+            errors: '',
+
+            nameField: {
+                identifier: 'name',
+                label: 'Name',
+                type: 'text',
+                value: '',
+                callback: 'updateNameFieldValue'
+            },
+            userNameField: {
+                identifier: 'username',
+                label: 'Username',
+                type: 'text',
+                value: '',
+                callback: 'updateUserNameFieldValue'
+            },
+            emailField: {
+                identifier: 'email',
+                label: 'Email',
+                type: 'text',
+                value: '',
+                callback: 'updateEmailFieldValue'
+            },
+            passwordField: {
+                identifier: 'password',
+                label: 'Password',
+                type: 'password',
+                value: '',
+                callback: 'updatePasswordFieldValue'
+            },
+            confirmPasswordField: {
+                identifier: 'passwordconfirm',
+                label: 'Confirm Password',
+                type: 'password',
+                value: '',
+                callback: 'updateConfirmPasswordFieldValue'
+            },
         };
     },
 
     methods: {
         handleRegistration() {
             axios.post('/api/register', {
-                name: this.name,
-                username: this.username,
-                email: this.email,
-                password: this.password,
-                confirm_password: this.passwordconfirm
+                name: this.nameField.value,
+                username: this.userNameField.value,
+                email: this.emailField.value,
+                password: this.passwordField.value,
+                confirm_password: this.confirmPasswordField.value
             }).then((response) => {
                 this.registerFormNotification = {
                     message: 'Success! Redirecting to Login screen...',
@@ -97,6 +156,26 @@ export default {
                     this.errors = error.response.data.errors;
                 }
             });
+        },
+
+        updateNameFieldValue(elValue) {
+            this.nameField.value = elValue;
+        },
+
+        updateUserNameFieldValue(elValue) {
+            this.userNameField.value = elValue;
+        },
+        
+        updateEmailFieldValue(elValue) {
+            this.emailField.value = elValue;
+        },
+
+        updatePasswordFieldValue(elValue) {
+            this.passwordField.value = elValue;
+        },
+
+        updateConfirmPasswordFieldValue(elValue) {
+            this.confirmPasswordField.value = elValue;
         }
     }
 }
