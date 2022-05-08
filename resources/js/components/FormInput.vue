fe4d3114907e5b71a0f5e4fe0c262e6a91b783c2
Adam Makinson <adam.levi.makinson@gmail.com>
2022-05-07 22:45:03 -0400

    Create FormInput component and update the LoginForm to use it.

---
+++ resources/js/components/FormInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label :for="identifier" class="block">
+            {{elementLabel}}: <span v-if="errors" class="text-red-600">{{errors[0]}}</span>
+        </label>
+        <input :type="type" :name="identifier" v-model="elementValue" :id="identifier" class="block border h-8 w-full pl-2" @change="handleChangeEvent">
+    </div>
+</template>
+
+<script>
+import eventBus from '../eventBus.js';
+
+export default {
+    props: [
+        'errors',
+        'type',
+        'identifier',
+        'elementLabel',
+        'value',
+        'callback'
+    ],
+
+    data() {
+        return {
+            elementValue: this.value,
+            elementCallback: this.callback
+        }
+    },
+
+    methods: {
+        handleChangeEvent(e) {
+
+            console.log(e);
+
+            eventBus.$emit('callback', this.elementCallback, this.elementValue);
+        }
+    }
+}
+</script>
--- resources/js/pages/LoginForm.vue
+++ resources/js/pages/LoginForm.vue
@@ -2,14 +2,26 @@
     <div class="flex flex-col justify-center h-screen">
         <div class="w-112 self-center">
             <h1 class="ml-4">Login</h1>
-            <div class="mx-4 mt-4 px-4 pb-4 border rounded-sm">
+            <div class="m-4 p-4 border rounded-sm flex flex-col flex-rows-2 gap-y-4">
                 <notification v-if="typeof loginFormNotification === 'object'" v-bind:notice="loginFormNotification"></notification>
-                <form id="loginForm">
-                    <label for="username" class="block mt-4">Username: <span v-if="errors.username" class="text-red-600">{{errors.username[0]}}</span></label>
-                    <input type="text" name="username" v-model="username" id="username" class="block border h-8 w-full">
-
-                    <label for="password" class="block mt-4">Password: <span v-if="errors.password" class="text-red-600">{{errors.password[0]}}</span></label>
-                    <input type="password" name="password" v-model="password" id="password" class="block border h-8 w-full">
+                <form id="loginForm" class="flex flex-col flex-rows-3 gap-y-4">
+                    <FormInput 
+                        :identifier="userNameField.identifier" 
+                        :type="userNameField.type" 
+                        :elementLabel="userNameField.label" 
+                        :errors="errors.username" 
+                        :value="userNameField.value" 
+                        :callback="userNameField.callback"
+                    ></FormInput>
+                    
+                    <FormInput 
+                        :identifier="passwordField.identifier" 
+                        :type="passwordField.type" 
+                        :elementLabel="passwordField.label" 
+                        :errors="errors.password" 
+                        :value="passwordField.value" 
+                        :callback="passwordField.callback"
+                    ></FormInput>
 
                     <Button bgColorClass="bg-blue-600" colorClass="text-white" callback="handleLogin">Login</Button>
                 </form>
@@ -24,19 +36,22 @@ import Notification from '../components/Notification.vue';
 import Link from '../components/Link.vue';
 import Button from '../components/Button.vue';
 import eventBus from '../eventBus.js';
+import FormInput from '../components/FormInput.vue';
 
 export default {
     components: {
-        Notification,
-        Link,
-        Button,
-        eventBus
-    },
+    Notification,
+    Link,
+    Button,
+    eventBus,
+    FormInput
+},
 
     created() {
+        // This is used with the button component. We pass the callback to the
+        // button component which fires off an event and the callback gets executed
+        // here.
         eventBus.$on('callback', (callback, args) => {
-            var fn = window[callback];
-
             // 'this' is the VueComponent object
             if(args){
                 this[callback](args);
@@ -48,10 +63,22 @@ export default {
 
     data() {
         return {
-            username: '',
-            password: '',
             loginFormNotification: '',
-            errors: ''
+            errors: '',
+            userNameField: {
+                identifier: 'username',
+                label: 'Username',
+                type: 'text',
+                value: '',
+                callback: 'updateUserNameFieldValue'
+            },
+            passwordField: {
+                identifier: 'password',
+                label: 'Password',
+                type: 'password',
+                value: '',
+                callback: 'updatePasswordFieldValue'
+            }
         }
     },
 
@@ -60,8 +87,8 @@ export default {
             
             // Send the POST request
             axios.post('/api/login', {
-                username: this.username,
-                password: this.password
+                username: this.userNameField.value,
+                password: this.passwordField.value
             }).then((response) => {
                 this.$store.commit('setCurrentUser', response.data.user);
                 this.$router.push('chores-list');
@@ -75,6 +102,14 @@ export default {
                     this.errors = error.response.data.errors;
                 }
             });
+        },
+
+        updateUserNameFieldValue(elValue) {
+            this.userNameField.value = elValue;
+        },
+
+        updatePasswordFieldValue(elValue) {
+            this.passwordField.value = elValue;
         }
     },
 
