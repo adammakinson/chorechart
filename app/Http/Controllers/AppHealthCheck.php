cf5f159efd8477e870ad56f7c2faa69922286157
Adam Makinson <adam.levi.makinson@gmail.com>
2022-06-12 23:53:23 -0400

    WIP - check for db config, db schema, user and roles during app health check

--- app/Http/Controllers/AppHealthCheck.php
+++ app/Http/Controllers/AppHealthCheck.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 
 class AppHealthCheck extends Controller
 {
@@ -34,13 +37,55 @@ class AppHealthCheck extends Controller
      * Do we want checking the app health and setting app config to
      * be separate things? Do we even want a user interface for setting
      * .env variables???
+     * 
+     * We need to check if an admin user exists too.
+     * 
+     * 1. Run a query checking for the database specified in the .env file
      */
     public function index()
     {
-        $appLoadable = true;
-        $databaseIsConfigured = true;
-        $initialUserIsSet = true;
+        $appLoadable = false;
+        $message = 'App is not loadable';
         
+        $databaseIsConfigured = $this->databaseIsConfigured();
+        if (!$databaseIsConfigured) {
+            $message = 'Looks like you haven\'t configured the database. Check your .env file';
+            return $message;
+        }
+
+        $schemaIsCorrect = $this->databaseSchemaIsCorrect();
+        if (!$schemaIsCorrect) {
+            $message = 'The database is not set up right. Did you run migrations?';
+            return $message;
+        }
+        
+        // So, first we need to check the users table for the existence of the
+        // users that should be put in place by the seeders. At minimum we need
+        // users and roles, I believe.
+        $initialUserIsSet = $this->initialAppUserIsConfigured();
+        if (!$initialUserIsSet) {
+            $message = 'You have not set your initial admin user. Check your .env file';
+            return $message;
+        }
+
+        $minimumRequiredSeedersRun = $this->initialAppUserExistsInDb() && $this->rolesExist() && $this->userRolesAssigned();
+        if (!$minimumRequiredSeedersRun) {
+            $message = 'You don\'t have your initial user completely set up. you may need to run your RoleSeeder and RoleUserSeeder.';
+        }
+
+        $appLoadable = $databaseIsConfigured && $schemaIsCorrect && $initialUserIsSet && $minimumRequiredSeedersRun;
+
+        if ($appLoadable) {
+            $message = 'App is loadable';
+        }
+
+        return $message;
+    }
+
+    private function databaseIsConfigured()
+    {
+        $databaseIsConfigured = true;
+
         $envDbConfig = [
             'DB_CONNECTION',
             'DB_DATABASE',
@@ -51,39 +96,98 @@ class AppHealthCheck extends Controller
             'FORSWARD_DB_PORT'
         ];
 
-        $initialAppUserConfig = [
-            'INITIAL_USER',
-            'INITIAL_USERNAME',
-            'INITIAL_USER_PASS'
-        ];
-
-        // Really, this check needs to occur after
-        // a check for the existence of the correct
-        // database schema.
         foreach ($envDbConfig as $dbConfig) {
             if (!env($dbConfig, false)) {
                 $databaseIsConfigured = false;
+                break;
             }
         }
+
+        return $databaseIsConfigured;
+    }
+
+    /**
+     * Check that all tables exist in the database
+     */
+    private function databaseSchemaIsCorrect(): bool
+    {
+        $schemaIsCorrect = true;
+
+        // It'd be nice if running migrations 
+        $dbTables = [
+            'chores',
+            'failed_jobs',
+            'images',
+            'migrations',
+            'password_resets',
+            'personal_access_tokens',
+            'rewards',
+            'role_user',
+            'roles',
+            'transactions',
+            'user_chores',
+            'users'
+        ];
+
+        foreach ($dbTables as $table) {
+            if (!Schema::hasTable($table)) {
+                $schemaIsCorrect = false;
+                break;
+            }
+        }
+
+        return $schemaIsCorrect;
+    }
+
+    private function initialAppUserIsConfigured()
+    {
+        $initialUserIsSet = true;
         
-        // It's not this simple. I also need to check
-        // if a user exists in the database.
+        $initialAppUserConfig = [
+            'INITIAL_USER_NAME',
+            'INITIAL_USER_USERNAME',
+            'INITIAL_USER_PASS',
+            'INITIAL_USER_EMAIL'
+        ];
+
         foreach ($initialAppUserConfig as $userConfig) {
             if (!env($userConfig, false)) {
                 $initialUserIsSet = false;
+                break;
             }
         }
 
-        $message = 'App is not loadable';
-        $appLoadable = $databaseIsConfigured && $initialUserIsSet;
+        return $initialUserIsSet;
+    }
 
-        // $config = $_ENV;
-        // $config = config('app.password_timeout');
+    private function initialAppUserExistsInDb()
+    {
+        $initialUser = DB::table('users')
+                                ->where('username', $_ENV['INITIAL_USER_USERNAME'])
+                                ->first();
 
-        if ($appLoadable) {
-            $message = 'App is loadable';
-        }
+        return $initialUser->username == $_ENV['INITIAL_USER_USERNAME'];
+    }
 
-        return $message;
+    private function rolesExist()
+    {
+        $roles = DB::table('roles')
+                    ->select('role')
+                    ->get();
+
+        $roleCount = $roles->count();
+
+        return $roleCount > 0;
+    }
+    
+    private function userRolesAssigned()
+    {
+        $initialUserRoles = DB::table('users')
+                                ->join('role_user', 'users.id', '=', 'role_user.user_id')
+                                ->where('users.username', $_ENV['INITIAL_USER_USERNAME'])
+                                ->select('roles.role')
+                                ->get();
+
+        return count($initialUserRoles) > 0;
     }
 }
