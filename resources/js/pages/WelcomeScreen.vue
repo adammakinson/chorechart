235bc92c3ca8810a96eb5df28c6ddfb0c288a7ac
Adam Makinson <adam.levi.makinson@gmail.com>
2022-08-01 22:32:36 -0400

    center title text

--- resources/js/pages/WelcomeScreen.vue
+++ resources/js/pages/WelcomeScreen.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col justify-center h-screen">
         <div class="flex flex-col items-center p-4">
-            <h1 class="mx-auto mb-4 text-2xl font-bold">Welcome to the chore chart!</h1>
+            <h1 class="mx-auto mb-4 text-2xl text-center font-bold">Welcome to the chore chart!</h1>
             <p class="mx-auto text-center">A great way to keep things going around the house!</p>
             <div class="mx-auto mt-4">
                 <Link href="/login">Login</Link>
