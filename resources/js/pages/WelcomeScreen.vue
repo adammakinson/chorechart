9e9b3b26e51b7a37a21af9af1549c2a449750c21
Adam Makinson <adam.levi.makinson@gmail.com>
2022-08-01 22:26:21 -0400

    fix welcome screen title font size

--- resources/js/pages/WelcomeScreen.vue
+++ resources/js/pages/WelcomeScreen.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col justify-center h-screen">
         <div class="flex flex-col items-center">
-            <h1 class="mx-auto mb-4 text-3xl font-bold">Welcome to the chore chart!</h1>
+            <h1 class="mx-auto mb-4 text-2xl font-bold">Welcome to the chore chart!</h1>
             <p class="mx-auto">A great way to keep things going around the house!</p>
             <div class="mx-auto mt-4">
                 <Link href="/login">Login</Link>
