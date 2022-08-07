13c0b2c99481707fd0d53e207f21b18bee386b96
Adam Makinson <adam.levi.makinson@gmail.com>
2022-08-07 17:26:03 -0400

    implement mitt instead of vue instance

--- resources/js/eventBus.js
+++ resources/js/eventBus.js
@@ -1,2 +1,13 @@
-import Vue from 'vue';
-export default new Vue();
+/**
+ * mitt: https://www.npmjs.com/package/mitt
+ */
+import mitt from 'mitt';
+
+let emitter = mitt();
+
+emitter.$on = emitter.on;
+emitter.$emit = emitter.emit;
+
+const eventBus = emitter;
+
+export default eventBus;
