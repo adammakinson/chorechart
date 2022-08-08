4af71777adcadb7e1a86f6077a8bd857f92bc598
Adam Makinson <adam.levi.makinson@gmail.com>
2022-08-07 22:20:30 -0400

    fix missing closing script tag

--- resources/js/components/Link.vue
+++ resources/js/components/Link.vue
@@ -1,5 +1,7 @@
 <template>
-    <a class="underline text-blue-600 hover:text-blue-400 visited:text-purple-600" href="href"><slot></slot></a>
+    <a class="underline text-blue-600 hover:text-blue-400 visited:text-purple-600" href="href">
+        <slot></slot>
+    </a>
 </template>
 
 <script>
@@ -8,3 +10,4 @@ export default {
         'href'
     ]
 }
+</script>
