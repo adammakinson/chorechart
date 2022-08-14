cdc942fc2d9d29d5c55fbf2e29589521b58c844f
Adam Makinson <adam.levi.makinson@gmail.com>
2022-08-14 14:42:55 -0400

    fix binding on the link component

--- resources/js/components/Link.vue
+++ resources/js/components/Link.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="underline text-blue-600 hover:text-blue-400 visited:text-purple-600" href="href">
+    <a class="underline text-blue-600 hover:text-blue-400 visited:text-purple-600" :href="href">
         <slot></slot>
     </a>
 </template>
