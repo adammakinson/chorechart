07fd47012fd8457c6cae4bd8965b418831d88341
Adam Makinson <adam.levi.makinson@gmail.com>
2022-09-11 18:45:31 -0400

    add migrations and seeders to the App health check test

--- tests/Feature/AppHealthCheckTest.php
+++ tests/Feature/AppHealthCheckTest.php
@@ -7,9 +7,16 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Database\Seeders\UsersTableSeeder;
+use Database\Seeders\RolesSeeder;
+use Database\Seeders\RoleUserSeeder;
 
 class AppHealthCheckTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    
     /**
      * A basic feature test example.
      *
@@ -17,6 +24,10 @@ class AppHealthCheckTest extends TestCase
      */
     public function test_app_health_check_successful()
     {
+        $this->seed(UsersTableSeeder::class);
+        $this->seed(RolesSeeder::class);
+        $this->seed(RoleUserSeeder::class);
+
         $response = $this->getJson('/api/healthcheck');
         
         $response->assertJson(fn (AssertableJson $json) =>
