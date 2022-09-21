00af5effb0d1701482dc60d941427373724936ba
Adam Makinson <adam.levi.makinson@gmail.com>
2022-09-20 23:32:17 -0400

    Fix rewards table seeder

--- database/seeders/rewards_table_seeder.php
+++ database/seeders/rewards_table_seeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
+use App\Models\Image AS ImageModel;
+use App\Models\Reward AS RewardsModel;
 
 class rewards_table_seeder extends Seeder
 {
@@ -19,9 +22,44 @@ class rewards_table_seeder extends Seeder
      */
     public function run()
     {
-        DB::table('rewards')->insert([
-            ['reward' => '30 minutes of Roblox time', 'point_value' => 30, 'created_at' => now(), 'updated_at' => now()],
-            ['reward' => 'One 30 minute episode', 'point_value' => 30, 'created_at' => now(), 'updated_at' => now()],
-        ]);
+        $seedData = [
+            [
+                'reward' => '30 minutes of Roblox time',
+                'point_value' => 30,
+                'file' => 'robloxtimetest.jpg',
+                'imgalt' => 'Roblox time'
+            ],
+            [
+                'reward' => 'One 30 minute episode',
+                'point_value' => 30,
+                'file' => 'episodetimetest.jpg',
+                'imgalt' => 'One TV Episode'
+            ]
+        ];
+        
+        foreach ($seedData as $seed) {
+            
+            $rewardInstance = new RewardsModel([
+                'reward' => $seed['reward'],
+                'point_value' => $seed['point_value']
+            ]);
+
+            $rewardInstance->save();
+
+            $imageSeed = UploadedFile::fake()->image($seed['file']);
+            $storageLocation = env('FILESYSTEM_DRIVER', 'public') . '/images';
+
+            $imageSeed->store($storageLocation);
+            
+            $extension = $imageSeed->getClientOriginalExtension();
+
+            $rewardImageInstance = new ImageModel;
+            $rewardImageInstance->reward_id = $rewardInstance->id;
+            $rewardImageInstance->path = $storageLocation;
+            $rewardImageInstance->filename = $imageSeed->hashName();
+            $rewardImageInstance->file_extension = $extension;
+            $rewardImageInstance->alt_text = $seed['imgalt'];
+            $rewardImageInstance->save();
+        }
     }
 }
