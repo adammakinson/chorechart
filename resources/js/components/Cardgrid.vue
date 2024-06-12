3c00f77298cd608aae423107573d1fb43cdd223c
Adam Makinson <adam.levi.makinson@gmail.com>
2024-06-12 00:03:58 -0400

    adjust user grid widths

--- resources/js/components/Cardgrid.vue
+++ resources/js/components/Cardgrid.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <slot></slot>
     </div>
 </template>
