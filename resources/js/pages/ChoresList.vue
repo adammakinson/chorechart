f8e5996c10fbf302aac9c383c161f596f695ddb2
Adam Makinson <adam.levi.makinson@gmail.com>
2024-07-14 21:45:19 -0400

    adjust padding

--- resources/js/pages/ChoresList.vue
+++ resources/js/pages/ChoresList.vue
@@ -3,7 +3,7 @@
         <title-bar></title-bar>
         <div class="grid transition-all duration-500 ease-in-out">
             <appmenu></appmenu>
-            <div class="p-5 w-full">
+            <div class="w-full">
                 <div v-if="!myChores || myChores.length == 0" class="grid h-screen justify-center items-center">
                     <div class="w-96 p-4 h-96">
                         <h2 v-if="!userIsAdmin" class="text-4xl text-center">You don't have any chores assigned to you. Check back later.</h2>
