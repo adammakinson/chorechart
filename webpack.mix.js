0a1a964a16ef55c3b8115cce3bd35622347219cf
Adam Makinson <adam.levi.makinson@gmail.com>
2024-07-16 18:14:42 -0400

    clean up mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,11 +18,3 @@ mix.alias({
 mix.js('resources/js/app.js', 'public/js')
     .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
     .vue();
-
-
-    // mix.sass('resources/sass/app.scss', 'public/css')
-    
-// .postCss('resources/css/app.css', 'public/css', [
-    //
-// ]);
-
