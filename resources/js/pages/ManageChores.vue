0ff179359a752a1d5f26fe09bbb982850c314322
Adam Makinson <adam.levi.makinson@gmail.com>
2024-07-17 18:28:11 -0400

    clean up unused or abandoned code

--- resources/js/pages/ManageChores.vue
+++ resources/js/pages/ManageChores.vue
@@ -3,9 +3,9 @@
         <title-bar></title-bar>
         <div class="grid min-h-screen transition-all duration-500 ease-in-out">
             <appmenu></appmenu>
-                <div id="choreManagement" class="p-5 w-full">
-                    <component :is="mainTabContents"></component>
-                </div>
+            <div id="choreManagement" class="p-5 w-full">
+                <component :is="mainTabContents"></component>
+            </div>
         </div>
     </div>
 </template>
@@ -13,45 +13,19 @@
 <script>
 import eventBus from '../eventBus';
 import Appmenu from "../components/AppMenu.vue";
-import TabComponent from "../components/TabComponent";
 import ManageChoresTabContents from "./ManageChoresTabContents.vue";
-import ViewAssignmentsTabContents from "./ViewAssignmentsTabContents.vue";
 import TitleBar from '../components/TitleBar.vue';
 
 export default {
 
     created() {
-        eventBus.on("manage-chores-tab-click", (contents) => {
-            this.mainTabContents = contents;
-        });
-        
-        eventBus.on("view-assignments-tab-click", (contents) => {
-            this.mainTabContents = contents;
-        });
-
         eventBus.on("mobileMainMenuIconClicked", () => {
             this.mainMenuIsOpen = !this.mainMenuIsOpen;
         });
-
-        this.mainMenuIsOpen = false;
     },
 
     data() {
         return {
-            mainTabsData: [{
-                'id': 1,
-                'label': 'Manage Chores',
-                'active': true,
-                'firesEvent': 'manage-chores-tab-click',
-                'loadsContent': 'ManageChoresTabContents'
-            },
-            {
-                'id': 2,
-                'label': 'View Assignments',
-                'firesEvent': 'view-assignments-tab-click',
-                'loadsContent': 'ViewAssignmentsTabContents'
-            }],
-
             mainTabContents: 'ManageChoresTabContents',
             mainMenuIsOpen: false
         }
@@ -60,7 +34,6 @@ export default {
     components: {
         Appmenu,
         ManageChoresTabContents,
-        ViewAssignmentsTabContents,
         TitleBar
     }
     
--- resources/js/pages/ViewAssignmentsTabContents.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>View assignments page</h1>
-</template>
