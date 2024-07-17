136fb255f0f5894cf41e40c6aed183950fc311f3
Adam Makinson <adam.levi.makinson@gmail.com>
2024-07-17 18:41:23 -0400

    simplify comments in app vue component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -18,20 +18,16 @@ import axios from 'axios';
 export default {
     created() {
 
-        // I'm thinking this could be the place where we check for the existence
-        // of the database schema we need for the app. The idea is a health check
-        // for the application; do we have the things we need in order for the app
-        // to run?
-
-        // I need a controller for sure. Not sure about models. Don't need any
-        // new migrations for this piece.
-
-        // I'm thinking the class could be the centralized piece, accessible by
-        // both console and http routes.
         this.checkAppHealth();
     },
 
     methods: {
+
+        /**
+         * This makes a call to the server side to check if the database config
+         * is set up, the initial user is defined, the migrations and seeders
+         * have been run.
+         */
         checkAppHealth() {
             axios.get('/api/healthcheck')
             .then((response) => {
