64166220ee5a8898ca0b633650966d0ebba0320a
Adam Makinson <adam.levi.makinson@gmail.com>
2024-07-30 20:33:20 -0400

    remove unused errors object

--- resources/js/pages/ManageAccount.vue
+++ resources/js/pages/ManageAccount.vue
@@ -63,7 +63,6 @@ export default {
             userData: {},
             userInfoNotification: '',
             userCredsNotification: '',
-            errors: {},
             mainMenuIsOpen: false,
 
             editUserForm: {
